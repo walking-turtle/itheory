57043df8fee4fc337d551b7c79c0cdcf34d8dfb6
neze <clement@neze.fr>
2018-01-25 20:20:40 +0100

    [limited_memory] Trainable predictor
    First version
    * Learner can read files and collect substrings
    * Predictor uses this information to build a prediction tree
    
    Data sizes, for reference:
    
    With a text of 4.7MB of english language, substrings of length 10 (that
    is the target for our prediction), the substrings tree takes 65M of raw
    json and 9.3M in compressed format. The resulting prediction tree takes
    50M/6.2M.
    
    Signed-off-by: neze <clement@neze.fr>

--- src/predictor/__init__.py
+++ src/predictor/__init__.py
@@ -1,139 +1,215 @@
 import json,time
-import progressbar
 
-class SafeDict(dict):
-    def __getitem__(self,key):
-        return dict.__getitem__(self,''.join(key))
-    def __setitem__(self,key,val):
-        return dict.__setitem__(self,''.join(key),val)
-    # def __missing__(self,key):
-        # return 0
-
-class Counters(object):
+class SubStrings:
     def __init__(self):
-        pass
-    def __getattribute__(self,x):
-        try:
-            return object.__getattribute__(self,x)
-        except AttributeError:
-            return 0
-    def __str__(self):
-        return json.dumps(self.__dict__)
+        self.t = dict()
 
-    def reduce(self):
-        dup = type(self)()
-        t = sum(map(lambda x: x[1], self.__dict__.items()))
-        for x,y in self.__dict__.items():
-            setattr(dup,x,y/t)
-        return dup
+    def add_string(self,s):
+        c = self.t
+        for x in s:
+            if x not in c:
+                c[x] = dict()
+            c = c[x]
+        c['__'] = c.get('__',0)+1
 
-class PredictableText:
-    def __init__(self,text):
-        if isinstance(text,str):
-            self.string=text
-        else:
-            raise TypeError
+    def iteritems(self,value=None,depth=None):
+        if value is None or depth is None:
+            value = self.t
+            depth = 0
+        for c in filter(lambda x: x!='__',value):
+            yield c,value[c],depth
+            for k,v,d in self.iteritems(value=value[c],depth=depth+1):
+                yield k,v,d
 
-    def __iter__(self):
-        return Predictor(self)
+    def walk(self,node,value='.',depth=-1,last=1,count=None):
+        yield value,count,depth,last
+        children = [ k for k in node if k != '__' ]
+        for c in children[:-1]:
+            for v,c,d,l in self.walk(node[c],value=c,depth=depth+1,last=0,count=node[c].get('__',None)):
+                yield v,c,d,l
+        for c in children[-1:]:
+            for v,c,d,l in self.walk(node[c],value=c,depth=depth+1,last=1,count=node[c].get('__',None)):
+                yield v,c,d,l
+
+    def show(self):
+        res = ''
+        prefixes = list()
+        for value,count,depth,last in self.walk(self.t):
+            if depth >= 0:
+                while depth >= len(prefixes):
+                    prefixes.append('|  ')
+                if last:
+                    prefixes[depth] = '   '
+                else:
+                    prefixes[depth] = '|  '
+                res += ''.join(prefixes[:depth]) + ('└' if last else '├') + '─ ' + '{:}'.format(value) + (' ({:d})'.format(count) if count is not None else '') + '\n'
+            else:
+                res += value + '\n'
+        print(res)
+
+    def dump(self,s):
+        json.dump(self.t,s,separators=(',',':'))
+
+    def load(self,s):
+        self.t = json.load(s)
+
+    def update(self,SS):
+        type(self).merge(src=SS.ss,dst=self.ss)
+
+    @classmethod
+    def merge(cls,src=dict(),dst=dict()):
+        for k,v in src.items():
+            if k == '__':
+                dst['__'] = dst.get('__',0) + v
+                continue
+            if k not in dst:
+                dst[k] = dict()
+            cls.merge(src=v,dst=dst[k])
+
+class Learner:
+    default_buffer_size = 5
+    def __init__(self,buffer_size=None):
+        if buffer_size is None:
+            buffer_size = type(self).default_buffer_size
+        self.ss = SubStrings()
+        self.bs = buffer_size
+
+    @classmethod
+    def from_backup(cls,filename,*args,**kwargs):
+        self = cls(*args,**kwargs)
+        with open(filename,'r') as f:
+            return self.load(f)
+
+    def save_backup(self,filename):
+        with open(filename,'w') as f:
+            return self.dump(f)
+
+    def read_text(self,data):
+        for i in range(len(data)):
+            for j in range(min(self.bs,i)):
+                self.ss.add_string(data[i-j-1:i])
+        # for i in range(len(data)-self.bs):
+            # self.ss.add_string(data[i:i+self.bs])
+        return self
+
+    def read_file(self, filename):
+        if not isinstance(filename,str):
+            raise TypeError("<filename> should be a string")
+        with open(filename,'r') as f:
+            return self.read_text(f.read())
+
+    def dump(self,s):
+        self.ss.dump(s)
+        return self
+    def load(self,s):
+        self.ss.load(s)
+        return self
+
+class Predictions:
+    def __init__(self):
+        self.t = dict()
+
+    def add_prediction(self,s,p):
+        c = self.t
+        for x in reversed(s):
+            if x not in c:
+                c[x] = dict()
+            c = c[x]
+        c['__'] = p
+
+    def get_prediction(self,s):
+        c = self.t
+        for x in s[::-1]:
+            if x not in c:
+                break
+            c = c[x]
+        return c.get('__',None)
+
+    def walk(self,node,value='.',depth=-1,last=1,count=None):
+        yield value,count,depth,last
+        children = [ k for k in node if k != '__' ]
+        for c in children[:-1]:
+            for v,c,d,l in self.walk(node[c],value=c,depth=depth+1,last=0,count=node[c].get('__',None)):
+                yield v,c,d,l
+        for c in children[-1:]:
+            for v,c,d,l in self.walk(node[c],value=c,depth=depth+1,last=1,count=node[c].get('__',None)):
+                yield v,c,d,l
+
+    def dump(self,s):
+        json.dump(self.t,s,separators=(',',':'))
+
+    def load(self,s):
+        self.t = json.load(s)
+
+    def show(self):
+        res = ''
+        prefixes = list()
+        for value,count,depth,last in self.walk(self.t):
+            if depth >= 0:
+                while depth >= len(prefixes):
+                    prefixes.append('|  ')
+                if last:
+                    prefixes[depth] = '   '
+                else:
+                    prefixes[depth] = '|  '
+                res += ''.join(prefixes[:depth]) + ('└' if last else '├') + '─ ' + '{:}'.format(value) + (' ({:})'.format(count) if count is not None else '') + '\n'
+            else:
+                res += value + '\n'
+        print(res)
 
 class Predictor:
-    def __init__(self,text):
-        self.text = text
-        self.string=text.string
-        self.stats = Counters()
-        self.c = 0
-        self.l = len(self.string)
-        self.bar = progressbar.ProgressBar(max_value=self.l,redirect_stdout=True,\
-                widgets=['[',Widget(self),'] ',\
-                progressbar.Percentage(),' (',progressbar.Counter(),' of {:d}) '.format(self.l),\
-                progressbar.Bar(),' ',progressbar.Timer()])
-        self.n = None
-        self.δcount = 0
-        self.δ = 0.1
+    def __init__(self):
+        self.ps = Predictions()
 
-    def predict(self,l=None):
-        if l is None:
-            return None
-        """
-        Initialize loop:
-          * we are looking for the indices of the end of
-            patterns of length 1
-          * the only element to look at is the last one
-            of the pattern (controlled_element)
-        """
-        pattern_length = 1
-        controlled_element = self.string[l-pattern_length]
-        end_pattern_indices = set()
-        tmp_end_pattern_indices = {\
-                i + pattern_length - 1\
-                for i in range(l-pattern_length)\
-                if self.string[i] is controlled_element\
-                }
-        while len(tmp_end_pattern_indices):
-            del end_pattern_indices
-            end_pattern_indices = tmp_end_pattern_indices
-            pattern_length += 1
-            controlled_element = self.string[l-pattern_length]
-            tmp_end_pattern_indices = {\
-                    i + pattern_length - 1\
-                    for i in range(l-pattern_length)\
-                    if self.string[i] is controlled_element\
-                    } & end_pattern_indices
-        if len(end_pattern_indices):
-            return self.string[max(end_pattern_indices)+1]
-        return None
+    @classmethod
+    def from_substrings(cls,ss,*args,**kwargs):
+        self = cls(*args,**kwargs)
+        path = list()
+        for k,v,d in ss.iteritems():
+            while d >= len(path):
+                path.append('xxx')
+            path[d] = k
+            children = list(filter(lambda x: x!='__' and '__' in v[x], v))
+            children.sort()
+            if len(children):
+                # print(''.join(path[:d+1]) + ' has leafs')
+                # print({ c:v[c]['__'] for c in children })
+                m,c = 0,'xxxxx'
+                for i in children:
+                    if v[i]['__'] > m:
+                        c = i
+                        m = v[i]['__']
+                # print('best leaf is {:}'.format(c))
+                self.ps.add_prediction(path[:d+1],c)
+        return self
 
-    def update_delta(self):
-        if self.δcount > 10 and self.δ < 100:
-            self.δcount = 0
-            self.δ *= 10.
-        else:
-            self.δcount += 1
+    def predict(self,s):
+        return self.ps.get_prediction(s)
 
-    def update_bar(self):
-        t = time.time()
-        if self.n is None or ((t-self.n) > self.δ):
-            self.bar.update(min(self.c,self.l))
-            self.n = t
-            self.update_delta()
+    def read_text(self,text):
+        c = 0
+        d = 0
+        for i in range(len(text)):
+            d += 1
+            if text[i] == self.ps.get_prediction(text[max(0,i-10):i]):
+                c += 1
+            if i % 5000 == 1:
+                print('{:.4g}%'.format(100*(c/d)))
 
-    def __next__(self):
-        if self.c > self.l:
-            self.text.stats = self.stats.reduce()
-            self.bar.update(self.l)
-            raise StopIteration
-        p = self.predict(self.c)
-        if self.c < self.l:
-            x = self.string[self.c]
-            if p is None:
-                self.stats.unable += 1
-            elif p is x:
-                self.stats.correct += 1
-            else:
-                self.stats.wrong += 1
-        self.update_bar()
-        self.c += 1
-        return p
+    @classmethod
+    def from_backup(cls,filename,*args,**kwargs):
+        self = cls(*args,**kwargs)
+        with open(filename,'r') as f:
+            return self.load(f)
 
-class Widget:
-    def __init__(self,p):
-        self.predictor = p
+    def save_backup(self,filename):
+        with open(filename,'w') as f:
+            return self.dump(f)
 
-    def __call__(self,*args):
-        return '{:08.04f}% correct'.format(100*self.predictor.stats.reduce().correct)
+    def dump(self,s):
+        self.ps.dump(s)
+        return self
 
-def read_file(f):
-    with open(f,'r') if isinstance(f,str) else f as the_input:
-        t = PredictableText(the_input.read())
-    for _ in t:
-        pass
-    return t.stats
-
-def test_predictor():
-    l = "aabaabcaabaabc"
-    p = "xxaxbbaxbbcabc"
-    pred = Predictor()
-    for x,y in zip(l,p):
-        assert(y == 'x' or pred.predict() == y)
-        pred.append(x)
+    def load(self,s):
+        self.ps.load(s)
+        return self
