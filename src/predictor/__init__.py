8683defabb3eccb99b6ee00b867d6500bd54a44a
neze <clement@neze.fr>
2018-01-25 23:19:15 +0100

    [limited_memory] Better organization
    Signed-off-by: neze <clement@neze.fr>

--- src/predictor/__init__.py
+++ src/predictor/__init__.py
@@ -1,40 +1,96 @@
 import json,time
 
-class SubStrings:
+class TreeData:
     def __init__(self):
-        self.t = dict()
+        self.tree = dict()
 
-    def add_string(self,s):
-        c = self.t
-        for x in s:
-            if x not in c:
-                c[x] = dict()
-            c = c[x]
-        c['__'] = c.get('__',0)+1
+    def run(self,*args,**kwargs):
+        raise NotImplementedError
+
+    def add(self,*args,**kwargs):
+        raise NotImplementedError
+
+    def get(self,sequence):
+        raise NotImplementedError
+
+    def parse(self,data):
+        raise NotImplementedError
+
+    def parsef(self,filename):
+        with open(filename,'r') as stream:
+            self.parse(stream.read())
+
+    def dump(self,stream):
+        json.dump(self.tree,stream,separators=(',',':'))
+
+    def dumps(self):
+        return json.dumps(self.tree,separators=(',',':'))
+
+    def dumpf(self,filename):
+        with open(filename,'w') as stream:
+            self.dump(stream)
+
+    def post_load_hook(self):
+        raise NotImplementedError
+
+    def load(self,stream):
+        new = json.load(stream)
+        del self.tree
+        self.tree = new
+        self.post_load_hook()
+
+    def loads(self,string):
+        new = json.loads(string)
+        del self.tree
+        self.tree = new
+        self.post_load_hook()
+
+    def loadf(self,filename):
+        with open(filename,'r') as stream:
+            self.load(stream)
+        self.post_load_hook()
+
+    @classmethod
+    def merge(cls,src=dict(),dst=dict()):
+        raise NotImplementedError
+
+    def update(self,other):
+        type(self).merge(src=other.tree,dst=self.tree)
 
     def iteritems(self,value=None,depth=None):
         if value is None or depth is None:
-            value = self.t
+            value = self.tree
             depth = 0
         for c in filter(lambda x: x!='__',value):
             yield c,value[c],depth
             for k,v,d in self.iteritems(value=value[c],depth=depth+1):
                 yield k,v,d
 
-    def walk(self,node,value='.',depth=-1,last=1,count=None):
-        yield value,count,depth,last
-        children = [ k for k in node if k != '__' ]
+    def depth(self):
+        return max(d for k,v,d in self.iteritems())
+
+    def __len__(self):
+        return sum(1 for _ in self.iteritems())
+
+    def __walk(self,key='.',value=None,depth=None,last=1,label=None):
+        if value is None or depth is None:
+            value = self.tree
+            depth = -1
+        yield key,label,depth,last
+        children = list(filter(lambda x: x!='__', value))
         for c in children[:-1]:
-            for v,c,d,l in self.walk(node[c],value=c,depth=depth+1,last=0,count=node[c].get('__',None)):
-                yield v,c,d,l
+            for k,lb,d,l in self.__walk(key=c,value=value[c],\
+                    depth=depth+1,last=0,label=value[c].get('__',None)):
+                yield k,lb,d,l
         for c in children[-1:]:
-            for v,c,d,l in self.walk(node[c],value=c,depth=depth+1,last=1,count=node[c].get('__',None)):
-                yield v,c,d,l
+            for k,lb,d,l in self.__walk(key=c,value=value[c],\
+                    depth=depth+1,last=1,label=value[c].get('__',None)):
+                yield k,lb,d,l
 
     def show(self):
         res = ''
         prefixes = list()
-        for value,count,depth,last in self.walk(self.t):
+        for key,label,depth,last in self.__walk():
             if depth >= 0:
                 while depth >= len(prefixes):
                     prefixes.append('|  ')
@@ -42,19 +98,34 @@ class SubStrings:
                     prefixes[depth] = '   '
                 else:
                     prefixes[depth] = '|  '
-                res += ''.join(prefixes[:depth]) + ('└' if last else '├') + '─ ' + '{:}'.format(value) + (' ({:d})'.format(count) if count is not None else '') + '\n'
+                res += ''.join(prefixes[:depth])
+                res += '└─ ' if last else '├─ '
+                res += '{:}'.format(key)
+                if label is not None:
+                    res += ' ({:})'.format(label)
             else:
-                res += value + '\n'
+                res += key
+            res += '\n'
         print(res)
 
-    def dump(self,s):
-        json.dump(self.t,s,separators=(',',':'))
+class SubStrings(TreeData):
+    def __init__(self,cache = 5):
+        super(SubStrings,self).__init__()
+        self.cache = cache
 
-    def load(self,s):
-        self.t = json.load(s)
+    def add(self,sequence):
+        node = self.tree
+        for x in sequence:
+            if x not in node:
+                node[x] = dict()
+            node = node[x]
+        node['__'] = node.get('__',0)+1
 
-    def update(self,SS):
-        type(self).merge(src=SS.ss,dst=self.ss)
+    def parse(self,data):
+        last = len(data)
+        for i in range(last):
+            for j in range(min(self.cache,i)):
+                self.add(data[i-j-1:i])
 
     @classmethod
     def merge(cls,src=dict(),dst=dict()):
@@ -66,150 +137,60 @@ class SubStrings:
                 dst[k] = dict()
             cls.merge(src=v,dst=dst[k])
 
-class Learner:
-    default_buffer_size = 5
-    def __init__(self,buffer_size=None):
-        if buffer_size is None:
-            buffer_size = type(self).default_buffer_size
-        self.ss = SubStrings()
-        self.bs = buffer_size
+    def post_load_hook(self):
+        print('Good call')
+        self.cache = max(self.cache,self.depth()+1)
+
+class Predictions(TreeData):
+    def __init__(self,cache = 5):
+        super(Predictions,self).__init__()
+        self.cache = cache
 
     @classmethod
-    def from_backup(cls,filename,*args,**kwargs):
-        self = cls(*args,**kwargs)
-        with open(filename,'r') as f:
-            return self.load(f)
-
-    def save_backup(self,filename):
-        with open(filename,'w') as f:
-            return self.dump(f)
-
-    def read_text(self,data):
-        for i in range(len(data)):
-            for j in range(min(self.bs,i)):
-                self.ss.add_string(data[i-j-1:i])
-        # for i in range(len(data)-self.bs):
-            # self.ss.add_string(data[i:i+self.bs])
-        return self
-
-    def read_file(self, filename):
-        if not isinstance(filename,str):
-            raise TypeError("<filename> should be a string")
-        with open(filename,'r') as f:
-            return self.read_text(f.read())
-
-    def dump(self,s):
-        self.ss.dump(s)
-        return self
-    def load(self,s):
-        self.ss.load(s)
-        return self
-
-class Predictions:
-    def __init__(self):
-        self.t = dict()
-
-    def add_prediction(self,s,p):
-        c = self.t
-        for x in reversed(s):
-            if x not in c:
-                c[x] = dict()
-            c = c[x]
-        c['__'] = p
-
-    def get_prediction(self,s):
-        c = self.t
-        for x in s[::-1]:
-            if x not in c:
-                break
-            c = c[x]
-        return c.get('__',None)
-
-    def walk(self,node,value='.',depth=-1,last=1,count=None):
-        yield value,count,depth,last
-        children = [ k for k in node if k != '__' ]
-        for c in children[:-1]:
-            for v,c,d,l in self.walk(node[c],value=c,depth=depth+1,last=0,count=node[c].get('__',None)):
-                yield v,c,d,l
-        for c in children[-1:]:
-            for v,c,d,l in self.walk(node[c],value=c,depth=depth+1,last=1,count=node[c].get('__',None)):
-                yield v,c,d,l
-
-    def dump(self,s):
-        json.dump(self.t,s,separators=(',',':'))
-
-    def load(self,s):
-        self.t = json.load(s)
-
-    def show(self):
-        res = ''
-        prefixes = list()
-        for value,count,depth,last in self.walk(self.t):
-            if depth >= 0:
-                while depth >= len(prefixes):
-                    prefixes.append('|  ')
-                if last:
-                    prefixes[depth] = '   '
-                else:
-                    prefixes[depth] = '|  '
-                res += ''.join(prefixes[:depth]) + ('└' if last else '├') + '─ ' + '{:}'.format(value) + (' ({:})'.format(count) if count is not None else '') + '\n'
-            else:
-                res += value + '\n'
-        print(res)
-
-class Predictor:
-    def __init__(self):
-        self.ps = Predictions()
-
-    @classmethod
-    def from_substrings(cls,ss,*args,**kwargs):
-        self = cls(*args,**kwargs)
+    def from_substrings(cls,substrings):
+        self = cls(cache = substrings.cache)
         path = list()
-        for k,v,d in ss.iteritems():
-            while d >= len(path):
+        for key,value,depth in substrings.iteritems():
+            while depth >= len(path):
                 path.append('xxx')
-            path[d] = k
-            children = list(filter(lambda x: x!='__' and '__' in v[x], v))
+            path[depth] = key
+            children = list(filter(lambda x: x!='__' and '__' in value[x], value))
             children.sort()
-            if len(children):
-                # print(''.join(path[:d+1]) + ' has leafs')
-                # print({ c:v[c]['__'] for c in children })
-                m,c = 0,'xxxxx'
-                for i in children:
-                    if v[i]['__'] > m:
-                        c = i
-                        m = v[i]['__']
-                # print('best leaf is {:}'.format(c))
-                self.ps.add_prediction(path[:d+1],c)
+            if children:
+                maximum, best_child = 0, 'xxx'
+                for child in children:
+                    if value[child]['__'] > maximum:
+                        best_child = child
+                        maximum = value[child]['__']
+                self.add(path[:depth+1], best_child)
         return self
 
-    def predict(self,s):
-        return self.ps.get_prediction(s)
+    def add(self,sequence,prediction):
+        node = self.tree
+        for x in reversed(sequence):
+            if x not in node:
+                node[x] = dict()
+            node = node[x]
+        node['__'] = prediction
 
-    def read_text(self,text):
-        c = 0
-        d = 0
-        for i in range(len(text)):
-            d += 1
-            if text[i] == self.ps.get_prediction(text[max(0,i-10):i]):
-                c += 1
+    def get(self,sequence):
+        node = self.tree
+        for x in sequence[::-1]:
+            if x not in node:
+                break
+            node = node[x]
+        return node.get('__',None)
+
+    def run(self,text):
+        correct = 0
+        total = len(text)
+        for i in range(total):
+            if text[i] == self.get(text[max(0,i-self.cache):i]):
+                correct += 1
             if i % 5000 == 1:
-                print('{:.4g}%'.format(100*(c/d)))
+                print('{:.4g}%'.format(100*(correct/(i+1))))
 
-    @classmethod
-    def from_backup(cls,filename,*args,**kwargs):
-        self = cls(*args,**kwargs)
-        with open(filename,'r') as f:
-            return self.load(f)
+    def post_load_hook(self):
+        print('Good call')
+        self.cache = max(self.cache,self.depth()+2)
 
-    def save_backup(self,filename):
-        with open(filename,'w') as f:
-            return self.dump(f)
-
-    def dump(self,s):
-        self.ps.dump(s)
-        return self
-
-    def load(self,s):
-        self.ps.load(s)
-        return self
